b623e6859b2ea45c30aa123696b1990d8017259e
TH-Raju <rajukhan894200@gmail.com>
2024-02-26 23:44:37 +0600

    How to use Image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -8,6 +8,19 @@ export default function App() {
       <View style={{ width: 200, height: 200, backgroundColor: "red" }} /> */}
       <Text style={{ fontWeight: "bold", fontSize: 22 }}>Hello Raju</Text>
       <Text style={{ color: "red", textTransform: "uppercase" }}>Second</Text>
+      {/* local image  */}
+      <Image
+        source={require("./assets/1656036010178.jpg")}
+        style={{ height: 200, width: 200 }}
+      />
+      {/* remote image  */}
+      {/* When use URI must be specified height and width */}
+      <Image
+        source={{
+          uri: "https://assets-global.website-files.com/610bb663a35dd3364ddbf08c/6240a218db7ffbb91faf1589_creating-hover-effect-tailwind-css.png",
+        }}
+        style={{ height: 200, width: 200 }}
+      />
     </View>
   );
 }
