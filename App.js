dd0bd85b5e61d13f17393b9fe10926d1b7306265
TH-Raju <rajukhan894200@gmail.com>
2024-02-26 23:39:13 +0600

    How to use View + Text

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Explore the React Native world!</Text>
-      <StatusBar style="auto" />
+      {/* <View style={{ width: 200, height: 200, backgroundColor: "blue" }} />
+      <View style={{ width: 200, height: 200, backgroundColor: "red" }} /> */}
+      <Text style={{ fontWeight: "bold", fontSize: 22 }}>Hello Raju</Text>
+      <Text style={{ color: "red", textTransform: "uppercase" }}>Second</Text>
     </View>
   );
 }
