0c7fa9100f0c0f9c6baffad3222db95699a290a8
TH-Raju <rajukhan894200@gmail.com>
2024-03-02 22:40:34 +0600

    [Done] style in react native

--- App.js
+++ App.js
@@ -2,13 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        height: 200,
-        width: 300,
-        backgroundColor: "red",
-      }}
-    >
+    <View style={styles.container}>
       <Text
         style={{
           fontWeight: "bold",
@@ -20,8 +14,33 @@ export default function App() {
       >
         Hello
       </Text>
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: 200,
+    width: 300,
+    padding: 20,
+    paddingTop: 40,
+  },
+  box1: {
+    height: 50,
+    width: 50,
+    backgroundColor: "yellow",
+  },
+  box2: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green",
+  },
+  box3: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
+  },
+});
