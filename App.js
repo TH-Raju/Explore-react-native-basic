f5aead36ab191a23f491e05c1f1f36aa338ec1c8
TH-Raju <rajukhan894200@gmail.com>
2024-03-02 22:33:34 +0600

    inline css implement

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View
+      style={{
+        height: 200,
+        width: 300,
+        backgroundColor: "red",
+      }}
+    >
+      <Text
+        style={{
+          fontWeight: "bold",
+          color: "white",
+          fontSize: 20,
+          textAlign: "center",
+          backgroundColor: "blue",
+        }}
+      >
+        Hello
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
